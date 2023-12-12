3c1a4564ba9d5bda515e675e5af88bdd13fe6b7d
Tommaso Clini <tommaso@localhost.localdomain>
2023-12-12 21:12:44 +0100

    Final version 1.0

--- ESP32_CAM/main/src/camera.c
+++ ESP32_CAM/main/src/camera.c
@@ -86,7 +86,7 @@ static camera_config_t camera_config = {
     .frame_size = FRAMESIZE_VGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
 
     .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
-    .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+    .fb_count = 2,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
